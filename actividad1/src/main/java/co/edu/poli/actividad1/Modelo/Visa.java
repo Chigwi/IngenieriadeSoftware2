46b55c60a68b150c6aede24ca5db765ec0654706
SysZ2211 <anothersamwithz@gmail.com>
2025-08-12 09:42:08 -0500

    metodo pojos visa

--- actividad1/src/main/java/co/edu/poli/actividad1/Modelo/Visa.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/Modelo/Visa.java
@@ -6,13 +6,6 @@ import java.util.*;
  * 
  */
 public class Visa {
-
-    /**
-     * Default constructor
-     */
-    public Visa() {
-    }
-
     /**
      * 
      */
@@ -38,4 +31,59 @@ public class Visa {
      */
     private Pasaporte pasaporte;
 
+	public Visa(String idVisa, Pais paisDestino, String fechaEmision, String fechaExpiracion, Pasaporte pasaporte) {
+		super();
+		this.idVisa = idVisa;
+		this.paisDestino = paisDestino;
+		this.fechaEmision = fechaEmision;
+		this.fechaExpiracion = fechaExpiracion;
+		this.pasaporte = pasaporte;
+	}
+
+	public String getIdVisa() {
+		return idVisa;
+	}
+
+	public void setIdVisa(String idVisa) {
+		this.idVisa = idVisa;
+	}
+
+	public Pais getPaisDestino() {
+		return paisDestino;
+	}
+
+	public void setPaisDestino(Pais paisDestino) {
+		this.paisDestino = paisDestino;
+	}
+
+	public String getFechaEmision() {
+		return fechaEmision;
+	}
+
+	public void setFechaEmision(String fechaEmision) {
+		this.fechaEmision = fechaEmision;
+	}
+
+	public String getFechaExpiracion() {
+		return fechaExpiracion;
+	}
+
+	public void setFechaExpiracion(String fechaExpiracion) {
+		this.fechaExpiracion = fechaExpiracion;
+	}
+
+	public Pasaporte getPasaporte() {
+		return pasaporte;
+	}
+
+	public void setPasaporte(Pasaporte pasaporte) {
+		this.pasaporte = pasaporte;
+	}
+
+	@Override
+	public String toString() {
+		return "Visa [idVisa=" + idVisa + ", paisDestino=" + paisDestino + ", fechaEmision=" + fechaEmision
+				+ ", fechaExpiracion=" + fechaExpiracion + ", pasaporte=" + pasaporte + "]";
+	}
+    
 }
