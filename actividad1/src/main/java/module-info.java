72f3e834f19553c93399b7ebf5157bcd468b28cc
Chigwi <carlos.a.gamboag@gmail.com>
2025-08-12 09:51:41 -0500

    Hice un refactor del paquete controlador para conectar con el paquete vista

--- actividad1/src/main/java/module-info.java
+++ actividad1/src/main/java/module-info.java
@@ -4,4 +4,7 @@ module co.edu.poli.actividad1.vista {
 
     opens co.edu.poli.actividad1.vista to javafx.fxml;
     exports co.edu.poli.actividad1.vista;
+    
+    opens co.edu.poli.actividad1.Controlador to javafx.fxml;
+    exports co.edu.poli.actividad1.Controlador;
 }
