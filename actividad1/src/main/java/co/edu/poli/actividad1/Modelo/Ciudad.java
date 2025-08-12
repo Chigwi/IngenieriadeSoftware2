96dae7bbbb78a84b981fe630d3311035e665d3b9
SysZ2211 <anothersamwithz@gmail.com>
2025-08-12 09:37:20 -0500

    Metodos pojos de ciudad.

--- actividad1/src/main/java/co/edu/poli/actividad1/Modelo/Ciudad.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/Modelo/Ciudad.java
@@ -8,12 +8,6 @@ import java.util.*;
 public class Ciudad {
 
     /**
-     * Default constructor
-     */
-    public Ciudad() {
-    }
-
-    /**
      * 
      */
     private String nombre;
@@ -33,4 +27,51 @@ public class Ciudad {
      */
     private boolean esCapital;
 
+	public Ciudad(String nombre, String codigoPostal, Pais pais, boolean esCapital) {
+		super();
+		this.nombre = nombre;
+		this.codigoPostal = codigoPostal;
+		this.pais = pais;
+		this.esCapital = esCapital;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCodigoPostal() {
+		return codigoPostal;
+	}
+
+	public void setCodigoPostal(String codigoPostal) {
+		this.codigoPostal = codigoPostal;
+	}
+
+	public Pais getPais() {
+		return pais;
+	}
+
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+
+	public boolean isEsCapital() {
+		return esCapital;
+	}
+
+	public void setEsCapital(boolean esCapital) {
+		this.esCapital = esCapital;
+	}
+
+	@Override
+	public String toString() {
+		return "Ciudad [nombre=" + nombre + ", codigoPostal=" + codigoPostal + ", pais=" + pais + ", esCapital="
+				+ esCapital + "]";
+	}
+
+    
 }
