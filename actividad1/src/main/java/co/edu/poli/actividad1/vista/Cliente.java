cfbc104f70ce3f6774a9720fa44de29839a9ced5
salitooooo <salodorado2004@gmail.com>
2025-08-14 13:04:19 -0500

    Instanciacion de pasaporte en el main de vista

--- actividad1/src/main/java/co/edu/poli/actividad1/vista/Cliente.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/vista/Cliente.java
@@ -4,14 +4,26 @@ import co.edu.poli.actividad1.Modelo.Pais;
 import co.edu.poli.actividad1.Modelo.Titular;
 import co.edu.poli.actividad1.Modelo.Pasaporte;
 import co.edu.poli.actividad1.Modelo.Visa;
+import java.util.ArrayList;
 
 public class Cliente {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		ArrayList <Ciudad> ciudades = new ArrayList <Ciudad>();
+		Ciudad medellin = new Ciudad ("medellin", "1", false );
+		ciudades.add(medellin);
 		
-		Ciudad medellin = new Ciudad ("medellin", "1", )
+		Pais colombia = new Pais (1,"colombia","espaniol", ciudades);
+		
+		Titular Allyson = new Titular ("allyson velandia","14/04/2002", "1089765678");
+		
+		Pasaporte AlliePasport = new Pasaporte ("AX400", colombia, "14/08/2025", "14/08/2035", Allyson, medellin);
+		
+		System.out.println(AlliePasport);
+		
+		
 	}
 	
 
