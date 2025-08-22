bad4ee925b90202e151b9baf449e5a0076e6344b
Chigwi <145591851+Chigwi@users.noreply.github.com>
2025-08-22 12:29:00 -0500

    DatabaseConnectio test #2

--- actividad1/src/main/java/co/edu/poli/actividad1/Repositorios/DatabaseConnection.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/Repositorios/DatabaseConnection.java
@@ -1,13 +1,14 @@
 package co.edu.poli.actividad1.Repositorios;
 
+import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://https://wndblehjsvhczsicivyf.supabase.co";
-        String user = "Chigwi";
+        String url = "jdbc:postgresql://postgres:Servidor123@db.wndblehjsvhczsicivyf.supabase.co:5432/postgres";
+        String user = "postgres";
         String password = "Servidor123";
         try (Connection conn = DriverManager.getConnection(url, user, password)) {
             System.out.println("Connected to the database!");
