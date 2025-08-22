49d82edf713d8d7c11017bb38e3849a6c8238644
salitooooo <salodorado2004@gmail.com>
2025-08-22 12:36:53 -0500

    intento de arreglo de conexion #3

--- actividad1/src/main/java/co/edu/poli/actividad1/Repositorios/DatabaseConnection.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/Repositorios/DatabaseConnection.java
@@ -6,9 +6,11 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://postgres:Servidor123@db.wndblehjsvhczsicivyf.supabase.co:5432/postgres";
-        String user = "postgres";
-        String password = "Servidor123";
+
+String url = "jdbc:postgresql://db.wndblehjsvhczsicivyf.supabase.co:5432/postgres";
+String user = "postgres";
+String password = "Servidor123";
+
         try (Connection conn = DriverManager.getConnection(url, user, password)) {
             System.out.println("Connected to the database!");
         } catch (SQLException e) {
