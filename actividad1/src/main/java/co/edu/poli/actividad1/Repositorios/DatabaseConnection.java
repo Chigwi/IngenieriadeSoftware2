553ce3b5fa3f6128187730aa28ec4a0cfed87923
Chigwi <145591851+Chigwi@users.noreply.github.com>
2025-08-22 11:55:57 -0500

    supabase connection test # 1

---
+++ actividad1/src/main/java/co/edu/poli/actividad1/Repositorios/DatabaseConnection.java
@@ -0,0 +1,18 @@
+package co.edu.poli.actividad1.Repositorios;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://https://wndblehjsvhczsicivyf.supabase.co";
+        String user = "Chigwi";
+        String password = "Servidor123";
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Connected to the database!");
+        } catch (SQLException e) {
+            System.out.println("Connection failed: " + e.getMessage());
+        }
+    }
+}
--- actividad1/src/main/java/module-info.java
+++ actividad1/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module co.edu.poli.actividad1.vista {
     requires javafx.controls;
     requires javafx.fxml;
+	requires java.sql;
 
     opens co.edu.poli.actividad1.vista to javafx.fxml;
     exports co.edu.poli.actividad1.vista;
