2107700a50f5af188c062585a17d3d46c5c8713c
Chigwi <145591851+Chigwi@users.noreply.github.com>
2025-08-24 17:11:53 -0500

    Metodo insert implementado correctamente

--- actividad1/src/main/java/co/edu/poli/actividad1/Repositorios/PasaporteDao.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/Repositorios/PasaporteDao.java
@@ -7,10 +7,39 @@ import java.sql.*;
 
 public class PasaporteDao implements Dao <Pasaporte>{
 
+	private Connection connection;
+	
+	public PasaporteDao (Connection connection) {
+		this.connection = connection;
+	}
+	
 	@Override
-	public String insert(Pasaporte t) {
-		// TODO Auto-generated method stub
-		return null;
+	public String insert(Pasaporte t){
+		String sql = "INSERT INTO \"Pasaporte\" (\"numeroId\", \"paisEmisor\", \"fechaEmision\", \"fechaExpiracion\", titular, \"ciudadEmision\") VALUES (?, ?, ?, ?, ?, ?)";
+		try (PreparedStatement pstmt = connection.prepareStatement(sql)){
+			pstmt.setString(1, t.getNumeroId());
+			
+			pstmt.setInt(2, t.getPaisEmisor().getIdPais());
+			
+			pstmt.setString(3, t.getFechaEmision());
+			
+			pstmt.setString(4, t.getFechaExpiracion());
+			
+			pstmt.setString(5, t.getTitular().getIdentificacion());
+			
+			pstmt.setString(6, t.getCiudadEmision().getCodigoPostal());
+			
+			pstmt.executeUpdate();
+			
+			return "Insercion exitosa!";
+			
+		}catch(SQLException e) {
+			
+			System.out.println("Error de insercion " + e.getMessage());
+			e.printStackTrace();
+			
+		}
+		return "";
 	}
 
 	@Override
--- actividad1/src/main/java/co/edu/poli/actividad1/vista/Cliente.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/vista/Cliente.java
@@ -4,25 +4,47 @@ import co.edu.poli.actividad1.Modelo.Pais;
 import co.edu.poli.actividad1.Modelo.Titular;
 import co.edu.poli.actividad1.Modelo.Pasaporte;
 import co.edu.poli.actividad1.Modelo.Visa;
+import co.edu.poli.actividad1.Repositorios.DatabaseConnection;
+import co.edu.poli.actividad1.Repositorios.PasaporteDao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Cliente {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//t
 		
-		ArrayList <Ciudad> ciudades = new ArrayList <Ciudad>();
-		Ciudad medellin = new Ciudad ("medellin", "1", false );
-		ciudades.add(medellin);
+    	/*String URL = "jdbc:postgresql://aws-1-us-east-2.pooler.supabase.com:5432/postgres";
+	    String USER = "postgres.wndblehjsvhczsicivyf";
+	    String PASSWORD = "Servidor123";*/
 		
-		Pais colombia = new Pais (1,"colombia","espaniol", ciudades);
-		
-		Titular Allyson = new Titular ("allyson velandia","14/04/2002", "1089765678");
-		
-		Pasaporte AlliePasport = new Pasaporte ("AX400", colombia, "14/08/2025", "14/08/2035", Allyson, medellin);
-		
-		System.out.println(AlliePasport);
+	    try(Connection conn = DatabaseConnection.getConnection()){
+	    	
+			ArrayList <Ciudad> ciudades = new ArrayList <Ciudad>();
+			
+			Ciudad medellin = new Ciudad ("medellin", "1", false );
+			
+			ciudades.add(medellin);
+			
+			Pais colombia = new Pais (1,"colombia","espaniol", ciudades);
+			
+			Titular Allyson = new Titular ("allyson velandia","14/04/2002", "1089765678");
+			
+			Pasaporte AlliePasport = new Pasaporte ("AX400", colombia, "14/08/2025", "14/08/2035", Allyson, medellin);
+			
+			//System.out.println(AlliePasport);
+			
+			PasaporteDao regPass = new PasaporteDao (conn);
+			
+			System.out.println(regPass.insert(AlliePasport));
+			
+	    }catch(SQLException e) {
+	    	
+	    }
+
 		
 		
 	}
