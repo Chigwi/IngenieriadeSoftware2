e84e37d1a0c8e63076107f7f695f08604df2ec03
Chigwi <145591851+Chigwi@users.noreply.github.com>
2025-08-25 15:00:33 -0500

    pruebas metodos select #2

--- actividad1/src/main/java/co/edu/poli/actividad1/vista/Cliente.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/vista/Cliente.java
@@ -48,7 +48,9 @@ public class Cliente {
 	    	
 			ArrayList <Ciudad> ciudades = new ArrayList <Ciudad>();
 			
-			Ciudad medellin = new Ciudad ("medellin", "1", false );
+			
+			
+			Ciudad medellin = new Ciudad ("0572", "Medellin", false );
 			
 			//System.out.println(regCiud.select(medellin.getCodigoPostal()));
 			
@@ -60,13 +62,17 @@ public class Cliente {
 			
 			//System.out.println(regPais.select(colombia.getIdPais()));
 			
-			Titular Allyson = new Titular ("allyson velandia","14/04/2002", "1089765678");
+			Titular Allyson = new Titular ("1000034908","14/04/2002", "Allyson Velandia");
 			
 			//System.out.println(regtit.select(Allyson.getIdentificacion()));
 			
 			Pasaporte AlliePasport = new Pasaporte ("AX400", colombia, "14/08/2025", "14/08/2035", Allyson, medellin);
 			
 			Pasaporte AlliePasporte = new Pasaporte ("AX400", colombia, "14/08/2025", "14/08/2045", Allyson, medellin);
+			
+			regPass.Delete(AlliePasport);
+			
+			regPass.insert(AlliePasport);
 			
 			//System.out.println(AlliePasport);
 			
@@ -86,21 +92,25 @@ public class Cliente {
 			
 			Ciudad Washington = new Ciudad ("0001", "washington", true);
 			
-			System.out.println(regCiud.insert(Washington));
+			System.out.println(regCiud.select(Washington.getCodigoPostal()));
 			
 			ArrayList <Ciudad> gringos = new ArrayList <Ciudad>();
 			
 			Pais EstadosUnidos = new Pais ("104", "EstadosUnidos", "ingles",gringos);
 			
-			System.out.println(regPais.insert(EstadosUnidos));
+			System.out.println(regPais.select(EstadosUnidos.getIdPais()));
 			
 			Titular SalomeDorado = new Titular ("1023530646", "17/03/04", "SalomeDorado");
 			
-			System.out.println(regtit.insert(SalomeDorado));
+			System.out.println(regtit.select(SalomeDorado.getIdentificacion()));
 			
 			Pasaporte SalomePasport = new Pasaporte ("SD2004",EstadosUnidos,"25/08/25", "25/08/35",SalomeDorado,Washington );
 			
-			System.out.println(regPass.insert(SalomePasport));
+			System.out.println(regPass.select(SalomePasport.getNumeroId()));
+			
+			System.out.println(regPass.selectAll());
+			
+			
 	    }catch(SQLException e) {
 	    	
 	    }
