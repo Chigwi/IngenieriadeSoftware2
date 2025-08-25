5400e9c29e0bd88f487ca084ffd6d93c7f7e67f5
Chigwi <145591851+Chigwi@users.noreply.github.com>
2025-08-25 13:13:08 -0500

    implementacion paisdao #1

--- actividad1/src/main/java/co/edu/poli/actividad1/Repositorios/PaisDao.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/Repositorios/PaisDao.java
@@ -1,20 +1,42 @@
 package co.edu.poli.actividad1.Repositorios;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import co.edu.poli.actividad1.Modelo.Pais;
 
 public class PaisDao implements Dao <Pais> {
+	
+	private Connection connection;
+	
+	public PaisDao (Connection connection) {
+		this.connection = connection;
+	}
 
 	@Override
 	public String insert(Pais t) {
-		// TODO Auto-generated method stub
+		String sql = "INSERT INTO \"Pais\" (\"idPais\", \"nombre\", \"idioma\") VALUES (?, ?, ?)";
+		try (PreparedStatement pstmt = connection.prepareStatement(sql)){
+			pstmt.setInt(1, t.getIdPais());
+			
+			pstmt.setString(2, t.getNombre());
+			
+			pstmt.setString(3, t.getIdioma());
+			
+			pstmt.executeUpdate();
+			
+		}catch (SQLException e) {
+			System.out.println("Error de insercion " + e.getMessage());
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public Pais select(String id) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
