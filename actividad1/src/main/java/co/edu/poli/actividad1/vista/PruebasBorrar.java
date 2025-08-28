695a4e99b4bcf4241724073c424202e8a8f7be09
salitooooo <salodorado2004@gmail.com>
2025-08-28 10:08:42 -0500

    main prueba borrar

--- actividad1/src/main/java/co/edu/poli/actividad1/vista/PruebasBorrar.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/vista/PruebasBorrar.java
@@ -1,5 +1,25 @@
 package co.edu.poli.actividad1.vista;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import co.edu.poli.actividad1.Modelo.Ciudad;
+import co.edu.poli.actividad1.Modelo.Pais;
+import co.edu.poli.actividad1.Modelo.Pasaporte;
+import co.edu.poli.actividad1.Modelo.Titular;
+import co.edu.poli.actividad1.Modelo.Visa;
+import co.edu.poli.actividad1.Repositorios.DatabaseConnection;
+import co.edu.poli.actividad1.Repositorios.VisaDao;
+
 public class PruebasBorrar {
+
+public static void main (String []args) {
+	
+	
+		
+	}
+		
+	
 
 }
