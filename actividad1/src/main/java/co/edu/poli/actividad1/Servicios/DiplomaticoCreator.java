8ba7a31a87c98cbf39303a8fa39a88bc9463d210
Chigwi <145591851+Chigwi@users.noreply.github.com>
2025-09-09 14:46:23 -0500

    métodos de creacion factorias implementados

--- actividad1/src/main/java/co/edu/poli/actividad1/Servicios/DiplomaticoCreator.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/Servicios/DiplomaticoCreator.java
@@ -1,13 +1,16 @@
 package co.edu.poli.actividad1.Servicios;
 
+import co.edu.poli.actividad1.Modelo.PDiplomatico;
 import co.edu.poli.actividad1.Modelo.Pasaporte;
 
 public class DiplomaticoCreator implements PCreator<Pasaporte>{
 
 	@Override
 	public Pasaporte createPasaporte() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		PDiplomatico p = new PDiplomatico(null, null, null, null, null, null, null);
+		
+		return p;
 	}
 
 }
--- actividad1/src/main/java/co/edu/poli/actividad1/Servicios/OrdinarioCreator.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/Servicios/OrdinarioCreator.java
@@ -1,13 +1,16 @@
 package co.edu.poli.actividad1.Servicios;
 
+import co.edu.poli.actividad1.Modelo.POrdinario;
 import co.edu.poli.actividad1.Modelo.Pasaporte;
 
 public class OrdinarioCreator implements PCreator<Pasaporte> {
 
 	@Override
 	public Pasaporte createPasaporte() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		POrdinario p = new POrdinario(null, null, null, null, null, null, null);
+		
+		return p;
 	}
 
 }
