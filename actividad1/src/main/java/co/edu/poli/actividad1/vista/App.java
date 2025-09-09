eebb46ba7536e45c283430b75928cb14997fd06b
Chigwi <145591851+Chigwi@users.noreply.github.com>
2025-09-09 15:12:36 -0500

    arreglo del metodo start para pantalla pasaporte

--- actividad1/src/main/java/co/edu/poli/actividad1/vista/App.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/vista/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,7 +20,8 @@ public class App extends Application {
     
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+    	AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("/co/edu/poli/actividad1/vista/primary.fxml"));
+        scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
--- actividad1/src/main/java/module-info.java
+++ actividad1/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module co.edu.poli.actividad1.vista {
     requires javafx.controls;
     requires javafx.fxml;
 	requires java.sql;
+	requires javafx.graphics;
 
     opens co.edu.poli.actividad1.vista to javafx.fxml;
     exports co.edu.poli.actividad1.vista;
