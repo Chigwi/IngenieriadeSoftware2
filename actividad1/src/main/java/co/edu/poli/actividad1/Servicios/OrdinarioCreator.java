980330f801f499b4d793f45f90988b6a314482d9
Chigwi <145591851+Chigwi@users.noreply.github.com>
2025-09-09 14:43:38 -0500

    creación de esqueleto de factorias

---
+++ actividad1/src/main/java/co/edu/poli/actividad1/Servicios/OrdinarioCreator.java
@@ -0,0 +1,13 @@
+package co.edu.poli.actividad1.Servicios;
+
+import co.edu.poli.actividad1.Modelo.Pasaporte;
+
+public class OrdinarioCreator implements PCreator<Pasaporte> {
+
+	@Override
+	public Pasaporte createPasaporte() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ actividad1/src/main/java/co/edu/poli/actividad1/Servicios/PCreator.java
@@ -0,0 +1,7 @@
+package co.edu.poli.actividad1.Servicios;
+
+public interface PCreator <Pasaporte>{
+	
+	public Pasaporte createPasaporte ();
+
+}
