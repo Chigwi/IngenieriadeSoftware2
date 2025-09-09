a2b0afbcd51d679a4d787083bd6bf6270d09a55d
salitooooo <salodorado2004@gmail.com>
2025-09-09 16:11:44 -0500

    limpieza de clase cliente

--- actividad1/src/main/java/co/edu/poli/actividad1/vista/Cliente.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/vista/Cliente.java
@@ -52,7 +52,7 @@ public class Cliente {
 			
 			Ciudad Bogotá = new Ciudad("0571", "Bogotá", true, "1");
 
-			//System.out.println(regCiud.select(medellin.getCodigoPostal()));
+			
 			
 			ciudades.add(medellin);
 			
@@ -60,43 +60,27 @@ public class Cliente {
 			
 			Pais colombia = new Pais ("1","colombia","espaniol", ciudades);
 			
-			//System.out.println(regPais.insert(colombia));
-			
-			//System.out.println(regPais.select(colombia.getIdPais()));
+		
 			
 
 			Titular Allyson = new Titular ("1000034908","14/04/2002", "Allyson Velandia");
-			//System.out.println(regtit.select(Allyson.getIdentificacion()));
+			
 			
 			//Pasaporte AlliePasport = new Pasaporte ("AX400", colombia, "14/08/2025", "14/08/2035", Allyson, medellin);
 			
 			//Pasaporte AlliePasporte = new Pasaporte ("AX400", colombia, "14/08/2025", "14/08/2045", Allyson, medellin);
 			
-			//regPass.Delete(AlliePasport);
-			
-			//regPass.insert(AlliePasport);
-			
-			//System.out.println(AlliePasport);
 			
 			
 			
-			//System.out.println(regPass.insert(AlliePasport));
 			
-			//System.out.println(regPass.select("AX400"));
 			
-			//System.out.println(regPass.Update(AlliePasporte));
-			
-			//System.out.println(regPass.select("AX400"));
-			
-			//System.out.println(regPass.Delete(AlliePasporte));
-			
-			//System.out.println(regPass.insert(AlliePasport));
 			
 			Ciudad Washington = new Ciudad ("0001", "washington", true,"104");
 			
 			Ciudad Miami = new Ciudad ("0002", "Miami", false, "104");
 			
-			//System.out.println(regCiud.select(Washington.getCodigoPostal()));
+			
 			
 			ArrayList <Ciudad> gringos = new ArrayList <Ciudad>();
 			
@@ -106,39 +90,29 @@ public class Cliente {
 			
 			Pais EstadosUnidos = new Pais ("104", "EstadosUnidos", "ingles",gringos);
 			
-			//System.out.println(regPais.select(EstadosUnidos.getIdPais()));
+			
 			
 			Titular SalomeDorado = new Titular ("1023530646", "17/03/04", "SalomeDorado");
 			
-			//System.out.println(regtit.select(SalomeDorado.getIdentificacion()));
+			
 			
 			//Pasaporte SalomePasport = new Pasaporte ("A2004",EstadosUnidos,"25/08/25", "25/08/35",SalomeDorado,Washington );
 			
-			//System.out.println(regPass.select(SalomePasport.getNumeroId()));
-			
-			//System.out.println(regPass.selectAll());
 			
 			//Visa v1 = new Visa ("VI001", EstadosUnidos,"28/08/2025","28/11/2025",AlliePasport);
 			
-			//System.out.println(regVis.insert(v1));
-			
-			//System.out.println(regVis.select(v1.getIdVisa()));
 			
 			//Visa v2 = new Visa ("VI001", EstadosUnidos,"28/08/2025","28/08/2035",AlliePasport);
 			
-			//System.out.println(regVis.Update(v2));
+	
 			
 			//Visa v3 = new Visa ("VI002", EstadosUnidos,"28/08/2025","28/08/2035",SalomePasport);
 			
-			//System.out.println(regVis.insert(v3));
-			
-			//System.out.println(regVis.selectAll());
+		
 			
 			//Visa v4 = new Visa ("VI003", EstadosUnidos,"28/08/2025","28/08/2035",SalomePasport);
 			
-			//System.out.println(regVis.insert(v4));
 			
-			//System.out.println(regVis.Delete(v4));
 			
 			ArrayList <Ciudad> ciudades1 = new ArrayList <Ciudad>();
 			
@@ -152,7 +126,7 @@ public class Cliente {
 			//Pasaporte SamPasaport = new Pasaporte ("AX1500", EstadosUnidos, "14/07/2024", "14/07/2045", Sam, Washington);
 			
 			
-			//System.out.println(regPass.selectIdFiltered("A"));
+		
 			
 			Ciudad Tunja = new Ciudad("0573", "Tunja", false, "1");
 			
