9523ae91b28a2c3da0a3507d02aa1550df81fe71
salitooooo <salodorado2004@gmail.com>
2025-09-09 14:58:10 -0500

    creacion de pantalla en scenbuilder con sus acciones e ids y cambio de esqueleto en el controlador

---
+++ actividad1/src/main/java/co/edu/poli/actividad1/Controlador/ControlPantallaPasaporte.java
@@ -0,0 +1,92 @@
+package co.edu.poli.actividad1.Controlador;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+public class ControlPantallaPasaporte {
+
+    @FXML
+    private Button bttInsertar;
+
+    @FXML
+    private Button bttShow;
+
+    @FXML
+    private Button bttactualizar;
+
+    @FXML
+    private Button bttdelete;
+
+    @FXML
+    private Button bttselect;
+
+    @FXML
+    private RadioButton inDiplomatico;
+
+    @FXML
+    private TextArea inExtra;
+
+    @FXML
+    private TextField inIdPasaporte;
+
+    @FXML
+    private RadioButton inOrdinario;
+
+    @FXML
+    private ComboBox<?> selectCiudad;
+
+    @FXML
+    private DatePicker selectFecha;
+
+    @FXML
+    private ComboBox<?> selectPais;
+
+    @FXML
+    private ComboBox<?> selectUsuarios;
+
+    @FXML
+    private ToggleGroup tipoPasaporte;
+
+    @FXML
+    void DeletePasaporte(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actualizarPasaporte(ActionEvent event) {
+
+    }
+
+    @FXML
+    void insertarPasaporte(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectPasaporte(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showSelect(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textDiplomatico(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textOrdinario(ActionEvent event) {
+
+    }
+
+}
