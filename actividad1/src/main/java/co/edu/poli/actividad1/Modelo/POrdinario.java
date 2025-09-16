5f9f3a24bd9ab8ee5f901a515d3dd915f6fd0928
SysZ2211 <anothersamwithz@gmail.com>
2025-09-16 13:09:04 -0500

    Implementacion de Builder a POrdinario.

--- actividad1/src/main/java/co/edu/poli/actividad1/Modelo/POrdinario.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/Modelo/POrdinario.java
@@ -4,10 +4,9 @@ public class POrdinario extends Pasaporte {
 	
 	private String razonViaje;
 
-	public POrdinario(String numeroId, Pais paisEmisor, String fechaEmision, String fechaExpiracion, Titular titular,
-			Ciudad ciudadEmision, String razonViaje, ElementoSeguridad es) {
-		super(numeroId, paisEmisor, fechaEmision, fechaExpiracion, titular, ciudadEmision, es);
-		this.razonViaje = razonViaje;
+	private POrdinario(Builder builder) {
+		super(builder.numeroId, builder.paisEmisor, builder.fechaEmision, builder.fechaExpiracion, builder.titular, builder.ciudadEmision, builder.es);
+		this.razonViaje = builder.razonViaje;
 	}
 
 	public String getRazonViaje() {
@@ -26,6 +25,52 @@ public class POrdinario extends Pasaporte {
 				+ ", CiudadEmision=" + getCiudadEmision().getNombre() + "]";
 	}
 	
-	
+	public static class Builder{
+		private String numeroId;
+		private Pais paisEmisor;
+		private String fechaEmision;
+		private String fechaExpiracion;
+		private Titular titular;
+		private Ciudad ciudadEmision;
+		private String razonViaje;
+		private ElementoSeguridad es;
+
+		public Builder agregarNumeroId(String numeroId){
+			this.numeroId = numeroId;
+			return this;
+		}
+		public Builder agregarPaisEmisor(Pais paisEmisor) {
+			this.paisEmisor= paisEmisor;
+			return this;
+		}
+		public Builder agregarFechaEmision(String fechaEmision) {
+			this.fechaEmision = fechaEmision;
+			return this;
+		}
+		public Builder agregarFecharExpiracion(String fechaExpiracion) {
+			this.fechaExpiracion = fechaExpiracion;
+			return this;
+		}
+		public Builder agregarTitular (Titular titular) {
+			this.titular = titular;
+			return this;
+		}
+		public Builder agregarCiudadEmision (Ciudad ciudadEmision) {
+			this.ciudadEmision = ciudadEmision;
+			return this;
+		}
+		public Builder agregarRazonViaje (String razonViaje) {
+			this.razonViaje = razonViaje;
+			return this;
+		}
+		public Builder agregarElementoSeguridad (ElementoSeguridad es) {
+			this.es	= es;
+			return this;
+		}
+		
+		public POrdinario build() {
+			return new POrdinario(this);
+		}
+	}
 
 }
