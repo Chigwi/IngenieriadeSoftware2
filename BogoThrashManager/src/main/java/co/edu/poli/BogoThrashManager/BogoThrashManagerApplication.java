4f031ac793a18d84a72eb87b1bee986d210e6cff
Chigwi <145591851+Chigwi@users.noreply.github.com>
2025-09-18 11:15:42 -0500

    añado plantilla base de proyecto BotgoTharashManager

---
+++ BogoThrashManager/src/main/java/co/edu/poli/BogoThrashManager/BogoThrashManagerApplication.java
@@ -0,0 +1,15 @@
+package co.edu.poli.BogoThrashManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BogoThrashManagerApplication {
+
+	public static void main(String[] args) {
+		
+		System.out.println("sapopinga!");
+		SpringApplication.run(BogoThrashManagerApplication.class, args);
+	}
+
+}
