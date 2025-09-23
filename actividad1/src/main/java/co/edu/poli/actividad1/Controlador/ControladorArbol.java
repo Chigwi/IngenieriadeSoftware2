b8effaafe6f2bdf88766a3e0e6ef28c5e0944a0c
Chigwi <145591851+Chigwi@users.noreply.github.com>
2025-09-23 08:53:13 -0500

    metodo initialize en control arbol

--- actividad1/src/main/java/co/edu/poli/actividad1/Controlador/ControladorArbol.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/Controlador/ControladorArbol.java
@@ -1,19 +1,28 @@
 package co.edu.poli.actividad1.Controlador;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import co.edu.poli.actividad1.vista.App;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
-public class ControladorArbol {
+public class ControladorArbol implements Initializable{
 
     @FXML
     private Button bttAtras;
 
     @FXML
     private Button bttMostrar;
+    
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
 
     @FXML
     void getNombre(ActionEvent event) {
@@ -25,5 +34,7 @@ public class ControladorArbol {
         App.setRoot("primary");
 
     }
+
+
 
 }
