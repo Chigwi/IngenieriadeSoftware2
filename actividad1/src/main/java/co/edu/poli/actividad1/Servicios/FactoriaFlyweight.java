81e5d2cfd151a84839aac278afcbe6498cd89bcd
SysZ2211 <anothersamwithz@gmail.com>
2025-10-23 14:06:48 -0500

    flyweight ez

--- actividad1/src/main/java/co/edu/poli/actividad1/Servicios/FactoriaFlyweight.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/Servicios/FactoriaFlyweight.java
@@ -1,5 +1,28 @@
 package co.edu.poli.actividad1.Servicios;
 
+import java.util.HashMap;
+
 public class FactoriaFlyweight {
 
+	private HashMap<String, PasaporteTipo> mapaTipos;
+
+	public FactoriaFlyweight(HashMap<String, PasaporteTipo> mapaTipos) {
+		super();
+		this.mapaTipos = mapaTipos;
+	}
+
+	public HashMap<String, PasaporteTipo> getMapaTipos() {
+		return mapaTipos;
+	}
+
+	public void setMapaTipos(HashMap<String, PasaporteTipo> mapaTipos) {
+		this.mapaTipos = mapaTipos;
+	}
+
+	@Override
+	public String toString() {
+		return "FactoriaFlyweight [mapaTipos=" + mapaTipos + "]";
+	}
+	
+	
 }
--- actividad1/src/main/java/co/edu/poli/actividad1/Servicios/PasaporteTipo.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/Servicios/PasaporteTipo.java
@@ -1,5 +1,54 @@
 package co.edu.poli.actividad1.Servicios;
 
+import co.edu.poli.actividad1.Modelo.Pasaporte;
+
 public class PasaporteTipo {
 
+	private String colorCubierta;
+	
+	private String idioma;
+	
+	private String paisEmisor;
+
+	public PasaporteTipo(String colorCubierta, String idioma, String paisEmisor) {
+		super();
+		this.colorCubierta = colorCubierta;
+		this.idioma = idioma;
+		this.paisEmisor = paisEmisor;
+	}
+
+	public String getColorCubierta() {
+		return colorCubierta;
+	}
+
+	public void setColorCubierta(String colorCubierta) {
+		this.colorCubierta = colorCubierta;
+	}
+
+	public String getIdioma() {
+		return idioma;
+	}
+
+	public void setIdioma(String idioma) {
+		this.idioma = idioma;
+	}
+
+	public String getPaisEmisor() {
+		return paisEmisor;
+	}
+
+	public void setPaisEmisor(String paisEmisor) {
+		this.paisEmisor = paisEmisor;
+	}
+
+	@Override
+	public String toString() {
+		return "PasaporteTipo [colorCubierta=" + colorCubierta + ", idioma=" + idioma + ", paisEmisor=" + paisEmisor
+				+ "]";
+	}
+	
+	public String mostrarDetalles(Pasaporte p) {
+		String message = p.toString() + "/n" + this.toString();
+		return message;
+	}
 }
