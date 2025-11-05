cd87fdef88bcb6bd59b089a1546b2328b3ad3534
SysZ2211 <anothersamwithz@gmail.com>
2025-11-05 11:22:40 -0500

    bases para el memento

---
+++ actividad1/src/main/java/co/edu/poli/actividad1/Servicios/CareTaker.java
@@ -0,0 +1,9 @@
+package co.edu.poli.actividad1.Servicios;
+
+import java.util.HashMap;
+
+public class CareTaker{
+	
+	private HashMap<String, AdaptadorPasaporte> Historial;
+
+}
---
+++ actividad1/src/main/java/co/edu/poli/actividad1/Servicios/ConcreteMemento.java
@@ -0,0 +1,7 @@
+package co.edu.poli.actividad1.Servicios;
+
+public class ConcreteMemento implements Memento{
+
+	private AdaptadorPasaporte state;
+	
+}
