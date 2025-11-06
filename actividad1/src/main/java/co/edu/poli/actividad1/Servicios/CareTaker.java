928997c1ee1c6acb5ca05dfe9f6165f7fe01845d
SysZ2211 <anothersamwithz@gmail.com>
2025-11-05 20:23:48 -0500

    CareTaker para revision

--- actividad1/src/main/java/co/edu/poli/actividad1/Servicios/CareTaker.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/Servicios/CareTaker.java
@@ -6,6 +6,20 @@ public class CareTaker{
 	
 	private HashMap<Integer, Memento> Historial;
 
+	public CareTaker() {
+		this.Historial = new HashMap<Integer, Memento>();
+	}
 	
+	public String addMemento(Integer key ,ConcreteMemento mm) {
+
+		this.Historial.put(key, mm);
+		
+		return "Insertado con llave " + key.toString();
+	}
 	
+	public Memento getMemento(Integer key) {
+		
+		return this.Historial.get(key);
+		
+	}
 }
