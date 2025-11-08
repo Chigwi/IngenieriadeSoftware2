e1f1c7a9c4753aff33749a014d3683bbbdb7e93d
SysZ2211 <anothersamwithz@gmail.com>
2025-11-08 18:45:08 -0500

    CommandManager base

---
+++ actividad1/src/main/java/co/edu/poli/actividad1/Servicios/CommandManager.java
@@ -0,0 +1,25 @@
+package co.edu.poli.actividad1.Servicios;
+
+public class CommandManager {
+
+	private Command comando;
+	
+	public CommandManager(Command comando) {
+		super();
+		this.comando = comando;
+	}
+
+	public void setCommand(Command comando) {
+		this.comando = comando;
+	}
+	
+	public String execute() {
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		return "CommandManager [comando=" + comando + "]";
+	}
+	
+}
