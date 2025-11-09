4c583058cfa4300d8c1c54afeb309ee28b326f66
SysZ2211 <anothersamwithz@gmail.com>
2025-11-09 11:05:18 -0500

    Asesor implementado

--- actividad1/src/main/java/co/edu/poli/actividad1/Servicios/Asesor.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/Servicios/Asesor.java
@@ -2,10 +2,24 @@ package co.edu.poli.actividad1.Servicios;
 
 public class Asesor implements Handler {
 
+	private Handler nextHandler;
+	
 	@Override
-	public String manejarSolicitud(Integer in) {
-		// TODO Auto-generated method stub
-		return null;
+	public String manejarSolicitud(Integer in, String sol) {
+	
+		if(nextHandler == null) {
+			if (in % 2 == 0) {
+				return sol + "\n solicitud aprobada por los asesores competentes";
+			}else {
+				return sol + "\n solicitud negada por los asesores competentes";
+			}	
+		}else {
+			if (in % 2 == 0) {
+				return sol + "\n solicitud aprobada por los asesores competentes " + nextHandler.manejarSolicitud(in, sol);
+			}else {
+				return sol + "\n solicitud negada por los asesores competentes";
+			}	
+		}
 	}
 
 }
