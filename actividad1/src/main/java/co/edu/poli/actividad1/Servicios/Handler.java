f5a6a77e1fd8c9c126c9977b737e788d943c0a35
salitooooo <salodorado2004@gmail.com>
2025-11-09 17:51:29 -0500

    indicacion de enlace con nect handler

--- actividad1/src/main/java/co/edu/poli/actividad1/Servicios/Handler.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/Servicios/Handler.java
@@ -1,7 +1,7 @@
 package co.edu.poli.actividad1.Servicios;
 
 public abstract class Handler {
-	
+	//direccionamiento a el iguiente handler 
 		private Handler nextHandler;
 
 		public String manejarSolicitud (Integer in, String sol) {
