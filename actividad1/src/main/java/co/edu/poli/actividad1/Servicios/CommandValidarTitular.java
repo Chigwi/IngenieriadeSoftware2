2b3b32d7bda670463f96bffdf604ab1787ada708
salitooooo <salodorado2004@gmail.com>
2025-11-08 19:22:46 -0500

    clase titularCommand

---
+++ actividad1/src/main/java/co/edu/poli/actividad1/Servicios/CommandValidarTitular.java
@@ -0,0 +1,19 @@
+package co.edu.poli.actividad1.Servicios;
+
+public class CommandValidarTitular implements Command{
+	
+	private Validacion validacion;
+
+	@Override
+	public String validate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public String validarTitular() {
+		return null;
+	}
+	
+	
+
+}
