21d73d806dec506ded773603ffd0c7368bd967c0
salitooooo <salodorado2004@gmail.com>
2025-11-11 16:56:24 -0500

    esqueleto mediator front end

---
+++ actividad1/src/main/java/co/edu/poli/actividad1/Controlador/ControladorNotificaciones.java
@@ -0,0 +1,45 @@
+package co.edu.poli.actividad1.Controlador;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class ControladorNotificaciones {
+
+    @FXML
+    private Button bttCancilleria;
+
+    @FXML
+    private Button bttMigracion;
+
+    @FXML
+    private Button bttPolicia;
+
+    @FXML
+    private Button bttSwitch;
+
+    @FXML
+    private TextArea inText;
+
+    @FXML
+    void notificarCancilleria(ActionEvent event) {
+
+    }
+
+    @FXML
+    void notificarMigracion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void notificarPolicia(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchToPrimary(ActionEvent event) {
+
+    }
+
+}
