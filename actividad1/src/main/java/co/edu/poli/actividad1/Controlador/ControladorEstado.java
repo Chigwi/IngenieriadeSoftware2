80efe0f0d3653cb0ee223774d56b4757e23064dd
salitooooo <salodorado2004@gmail.com>
2025-11-11 17:15:00 -0500

    esqueleto frontend state

---
+++ actividad1/src/main/java/co/edu/poli/actividad1/Controlador/ControladorEstado.java
@@ -0,0 +1,45 @@
+package co.edu.poli.actividad1.Controlador;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeView;
+
+public class ControladorEstado {
+
+    @FXML
+    private Button bttEstado;
+
+    @FXML
+    private Button bttSwitch;
+
+    @FXML
+    private ComboBox<?> selectEstado;
+
+    @FXML
+    private ComboBox<?> selectPais;
+
+    @FXML
+    private TextField showCurrent;
+
+    @FXML
+    private TreeView<?> showEstado;
+
+    @FXML
+    void cambiarEstado(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showEstado(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchToPrimary(ActionEvent event) {
+
+    }
+
+}
